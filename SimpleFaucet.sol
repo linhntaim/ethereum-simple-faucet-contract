6f261c54126de818d1a586b2f2cda0506ea1173d
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2023-07-02 21:56:52 +0700

    more logic

--- SimpleFaucet.sol
+++ SimpleFaucet.sol
@@ -1,51 +1,100 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
-* @title Simple Faucet
-* @dev Nguyen Tuan Linh <inbox@linhntaim.com>
-*/
+ * @title Simple Faucet
+ * @dev Nguyen Tuan Linh <inbox@linhntaim.com>
+ */
 contract SimpleFaucet is Ownable {
-    uint256 private _amount = 2 * 10**16;
-    mapping(address => uint) private _timeouts;
+    uint256 private _sendingAmount = (2 * 10**18) / 100;
+    uint256 private _maxDonatingAmount = 10 * 10**18;
+    uint256 private _delayMinutes = 180;
+    mapping(address => uint256) private _timeouts;
 
-    event Withdrawal(address indexed to, uint256 amount);
+    event Withdraw(address indexed to, uint256 amount);
     event Deposit(address indexed from, uint256 amount);
 
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    function getAmount() public view returns (uint256) {
-        return _amount;
+    function getSendingAmount() public view returns (uint256) {
+        return _sendingAmount;
     }
 
-    function withdraw() external {
+    function setSendingAmount(uint256 value) public onlyOwner {
+        require(
+            value >= 10**18 / 100 && value <= 10**18,
+            "Sending amount must be in range of [10^16 -> 10^18]."
+        );
+        _sendingAmount = value;
+    }
+
+    function getMaxDonatingAmount() public view returns (uint256) {
+        return _maxDonatingAmount;
+    }
+
+    function setMaxDonatingAmount(uint256 value) public onlyOwner {
+        require(
+            value >= 10**18 / 100 && value <= 100 * 10**18,
+            "Max donating amount must be in range of [10^16 -> 10^20]."
+        );
+        _maxDonatingAmount = value;
+    }
+
+    function getDelayMinutes() public view returns (uint256) {
+        return _delayMinutes;
+    }
+
+    function setDelayMinutes(uint256 value) public onlyOwner {
+        require(
+            value >= 15 && value <= 1440,
+            "Delay value must be in range of [15 -> 1440]."
+        );
+        _delayMinutes = value;
+    }
+
+    function sendMe() external {
         address sender = _msgSender();
         uint256 balance = getBalance();
-        uint256 amount = getAmount();
-        
-        require(balance >= amount, "This faucet is empty. Please check back later.");
-        require(_timeouts[sender] <= block.timestamp - 3 hours, "You can only withdraw once every 3 hours. Please check back later.");
-        
-        payable(sender).transfer(amount);
+        uint256 timeout = block.timestamp - _timeouts[sender];
+
+        require(
+            balance >= _sendingAmount,
+            "Fund is empty now. Please check back later."
+        );
+        require(
+            timeout >= _delayMinutes * 60,
+            string.concat(
+                "You can only get coins once every ",
+                Strings.toString(_delayMinutes),
+                " minutes. Please check back later."
+            )
+        );
+
+        payable(sender).transfer(_sendingAmount);
         _timeouts[sender] = block.timestamp;
-        
-        emit Withdrawal(sender, amount);
+
+        emit Withdraw(sender, _sendingAmount);
     }
-    
+
     receive() external payable {
         address sender = _msgSender();
-        uint256 depositAmount = msg.value;
-        uint256 withdrawAmount = getAmount();
+        uint256 donatingAmount = msg.value;
+
+        require(
+            donatingAmount <= _maxDonatingAmount,
+            "The donating amount is too big. Thanks but we do not receive that much."
+        );
 
         // If sender gives an amount of 75% withdrawal amount or bigger, the restriction time for withdrawing will be reset
-        if (depositAmount >= withdrawAmount / 4 * 3) {
-            _timeouts[sender] = block.timestamp - 3 hours;
+        if (donatingAmount >= (_sendingAmount / 4) * 3) {
+            _timeouts[sender] = block.timestamp - _delayMinutes * 60;
         }
 
-        emit Deposit(sender, depositAmount);
+        emit Deposit(sender, donatingAmount);
     }
 }
